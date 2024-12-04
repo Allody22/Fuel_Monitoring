bf8b2da3e6d6e2dfd7056fef90e73dbd94c7b79b
Allody <m.bogdanov2@g.nsu.ru>
2024-12-04 12:55:47 +0700

    vi -> v1

--- web/src/components/AuthService/AuthService.tsx
+++ web/src/components/AuthService/AuthService.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api/vi/auth',
+  baseURL: 'http://localhost:8080/api/v1/auth',
 });
 
 const AuthService = {
